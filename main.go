600032e02b444e87c8bb6081e98e32fbd88759ad
MalikTS <storm00606@gmail.com>
2026-05-05 00:36:18 +0300

    second commit

--- main.go
+++ main.go
@@ -1,1 +1,48 @@
 package main
+
+import "fmt"
+
+type Auto interface {
+	StepOnGaz()
+	StopOnBrake()
+}
+
+type BMW struct {
+}
+
+type Zhiga struct {
+}
+
+func (b BMW) StepOnGaz() {
+	fmt.Println("Я BMW! 550 лошадинных сил! Жмем на газ!")
+}
+func (b BMW) StopOnBrake() {
+	fmt.Println("Я BMW! торможу!")
+}
+
+
+
+func (zhiga Zhiga) StepOnGaz() {
+	fmt.Println("Я zhiga! 220 лошадинных сил! Жмем на газ!")
+}
+
+func (zhiga Zhiga) StopOnBrake() {
+	fmt.Println("Я zhiga! тормоза отказали")
+}
+
+func ride(auto Auto) {
+	fmt.Println("Я водитель!")
+	fmt.Println("Я сажусь в машину")
+	fmt.Println("И я нажимаю на газ...")
+	auto.StepOnGaz()
+	auto.StopOnBrake()
+}
+
+func main() {
+	bmw := BMW{}
+	zhiga := Zhiga{}
+	
+
+	ride(bmw)
+	ride(zhiga)
+}
